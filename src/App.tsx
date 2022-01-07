988f8afebdefb0bb92da832f42a5cc84763443d7
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2022-01-08 02:39:13 +0330

    remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ordersAsync, selectOrders } from "./store/orders/ordersSlice";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const orders = useSelector(selectOrders);
+
+  useEffect(() => {
+    dispatch(ordersAsync());
+  }, []);
+
+  useEffect(() => {
+    console.log(orders);
+  }, [orders]);
+
+  return <div></div>;
 }
 
 export default App;
