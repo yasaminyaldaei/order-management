b328b0cd8a5878453a68bfb7079b054b81c451ad
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2022-01-10 06:59:24 +0330

    add order details page

--- src/pages/OrderDetailsPage.tsx
+++ src/pages/OrderDetailsPage.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { OrderOverview } from "../components/OrderOverview";
+import { selectOrders } from "../store/orders/ordersSlice";
 
 export function OrderDetailsPage() {
-  const params = useParams();
+  const { orderId } = useParams();
+  const ordersList = useSelector(selectOrders);
+  const orderDetails = ordersList.find((order) => order.orderId === orderId);
+
+  if (!orderDetails) return null;
   return (
     <div>
-      <span>{params.orderId}</span>
+      <OrderOverview {...orderDetails} />
     </div>
   );
 }
--- src/pages/OrdersMainPage.tsx
+++ src/pages/OrdersMainPage.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { OrderOverview } from "../components/OrderOverview";
 import { ordersAsync, selectOrders } from "../store/orders/ordersSlice";
 import { Order } from "../types";
+import { GOTO_ORDER_DETAILS_PAGE } from "../routes";
 
 export function OrdersMainPage() {
   const dispatch = useDispatch();
@@ -20,7 +22,9 @@ export function OrdersMainPage() {
     <div>
       {ordersList && ordersList.length !== 0
         ? ordersList.map((order: Order) => (
-            <OrderOverview key={order.orderId} {...order} />
+            <Link to={GOTO_ORDER_DETAILS_PAGE(order.orderId)}>
+              <OrderOverview key={order.orderId} {...order} />
+            </Link>
           ))
         : null}
     </div>
